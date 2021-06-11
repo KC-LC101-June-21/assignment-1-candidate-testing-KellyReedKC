bde0a7596b2fd6df5c48b6ced582d9b024b78bc5
KellyReedKC <84790306+KellyReedKC@users.noreply.github.com>
2021-06-11 04:00:21 +0000

    correctly displayed array (almost)

--- candidate-testing.js
+++ candidate-testing.js
@@ -10,7 +10,7 @@ let candidateName ;
 let question = "Who was the first American woman in space? ";
 let correctAnswer = "Sally Ride";
 let candidateAnswer; 
-let questions;
+let questions = ["Who was the first American woman in space?", "True or false: 5 kilometer == 5000 meters?", "(5 + 3)/2 * 10 = ?", "Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2?", "What is the minimum crew size for the ISS?"];
 let correctAnswers;
 let candidateAnswers;
 
@@ -24,7 +24,11 @@ candidateName = input.question("What is the candidates name? ");
 
 function askQuestion() {
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
-candidateAnswer = input.question(question);
+
+
+for (let i = 0; i < questions.length; i++) {
+   console.log(questions[i]);
+}
 
 }
 
