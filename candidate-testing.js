353db22a2624c017d7c0d866df85b2205d3cc4f0
KellyReedKC <84790306+KellyReedKC@users.noreply.github.com>
2021-06-17 22:27:47 +0000

    Almost done

--- candidate-testing.js
+++ candidate-testing.js
@@ -13,6 +13,7 @@ let candidateAnswer;
 let questions = ["1) Who was the first American woman in space? \n", "2) True or false: 5 kilometer == 5000 meters? \n", "3) (5 + 3)/2 * 10 = ? \n", "4) Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? \n", "5) What is the minimum crew size for the ISS? \n"];
 let correctAnswers = ["Sally Ride", "true", "40", "Trajectory", "3"];
 let candidateAnswers = []
+let candidateGrade = 0
 
 
 function askForName() {
@@ -27,45 +28,58 @@ function askQuestion() {
 
   for (let i = 0; i < questions.length; i++) {
     candidateAnswers.push(input.question(questions[i]))
+
+    console.log(`Your answer: ${candidateAnswers[i]} \nCorrect answer: ${correctAnswers[i]}\n`);
+
   }
 }
 
-
 function gradeQuiz(candidateAnswers) {
 
+  let grade = 0
+  for (let i = 0; i < questions.length; i++) {
+  
+    if (correctAnswers[i].toLowerCase() === candidateAnswers[0].toLowerCase) {
+      grade += 1
+    }
+  }
 
-  // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
+    return grade;
+  }
 
-  //Replace the basic feedback from TODO 1.2c with a template literal that displays each of the candidate's responses in addition to the corresponding correct answers.//
+  function runProgram() {
+    askForName();
+    // TODO 1.1c: Ask for candidate's name //
 
+    // console.log("What's up " + candidateName);//
 
+    askQuestion();
+   
+  candidateGrade = gradeQuiz(this.candidateAnswers);
 
-  let grade;
+  candidateGrade = candidateGrade/5 * 100
 
+console.log(`>>>Overall grade: ${candidateGrade}% (${candidateGrade/100 * 5} of 5 responses correct<<<`);
 
-  return grade;
-}
+  if (candidateGrade => 80) {
+    console.log(">>>Status: PASSED<<<");}
+  
+  else {
+    console.log(">>>Status: FAILED<<<");
+  }
 
-function runProgram() {
-  askForName();
-  // TODO 1.1c: Ask for candidate's name //
+  }
 
-  // console.log("What's up " + candidateName);//
-
-  askQuestion();
-  gradeQuiz(this.candidateAnswers);
-}
-
-// Don't write any code below this line //
-// And don't change these or your program will not run as expected //
-module.exports = {
-  candidateName: candidateName,
-  question: question,
-  correctAnswer: correctAnswer,
-  candidateAnswer: candidateAnswer,
-  questions: questions,
-  correctAnswers: correctAnswers,
-  candidateAnswers: candidateAnswers,
-  gradeQuiz: gradeQuiz,
-  runProgram: runProgram
-};
+  // Don't write any code below this line //
+  // And don't change these or your program will not run as expected //
+  module.exports = {
+    candidateName: candidateName,
+    question: question,
+    correctAnswer: correctAnswer,
+    candidateAnswer: candidateAnswer,
+    questions: questions,
+    correctAnswers: correctAnswers,
+    candidateAnswers: candidateAnswers,
+    gradeQuiz: gradeQuiz,
+    runProgram: runProgram
+  };
